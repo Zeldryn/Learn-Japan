180c713840a6ae29c2fb09c8440e041fba3d089e
Zeldryn <ahm.riz666@gmail.com>
2026-03-10 23:42:34 +0700

    feat : update button background design

--- Widget/trueLogin.py
+++ Widget/trueLogin.py
@@ -2,6 +2,8 @@ from PySide6.QtWidgets import QWidget,QApplication,QLabel,QPushButton,QVBoxLayou
 from PySide6.QtCore import Qt, QPropertyAnimation, QPoint,QTimer, QUrl
 from Widget.Display import Display
 from Widget.Button import Button
+from Widget.Label import Label
+
 class Main(QWidget):
     def __init__(self,wLogin):
         super().__init__(wLogin)
@@ -15,18 +17,24 @@ class Main(QWidget):
                             border: none 2px;
                             border-radius:10px;
                             }""")
+ 
         
 
         self.uDisplay = Display("usernameDisplay",self)
         self.pDisplay = Display("passwordDisplay",self)
-        self.sButton = Button("submitButton",self)
-        self.rButton = Button("registButton",self)
+        self.sButton = Button("submitButton",self,self.uDisplay,self.pDisplay)
+        self.rButton = Button("registButton",self,None,None)
+        self.tfLogin = Label("failLogin",self)
+
         self.truloginLayout = QVBoxLayout(self)
-        self.truloginLayout.setContentsMargins(10,140,10,10)
+        self.truloginLayout.setSpacing(20)
+        self.truloginLayout.setContentsMargins(10,50,10,50)
         self.truloginLayout.addWidget(self.uDisplay,alignment=Qt.AlignCenter)
         self.truloginLayout.addWidget(self.pDisplay,alignment=Qt.AlignCenter)
+        self.truloginLayout.addWidget(self.tfLogin,alignment=Qt.AlignCenter)
         self.truloginLayout.addStretch()
-        self.truloginLayout.addWidget(self.sButton,alignment=Qt.AlignCenter)
-        self.truloginLayout.addWidget(self.rButton,alignment=Qt.AlignCenter)
 
-        self.truloginLayout.addStretch()
+        self.truloginLayout.addWidget(self.sButton,alignment=Qt.AlignCenter | Qt.AlignBottom)
+        self.truloginLayout.addWidget(self.rButton,alignment=Qt.AlignCenter | Qt.AlignBottom)
+
+
