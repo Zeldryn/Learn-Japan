f1a1fbf5f1b8ae12bc0332b07561d274e9fcc19d
Zeldryn <ahm.riz666@gmail.com>
2026-03-10 23:34:16 +0700

    feat : add Login and Register Button Logic

--- Pages/Login.py
+++ Pages/Login.py
@@ -30,15 +30,19 @@ class Login(QWidget):
         self.loginLayout = QVBoxLayout(self)
         margins = self.loginLayout.contentsMargins()
         self.loginLayout.setContentsMargins(15,90,15,100)
-        self.loginLayout.setSpacing(10)
-        self.loginLayout.addStretch
+        self.loginLayout.setSpacing(20)
+        self.loginLayout.addStretch()
         self.loginLayout.addWidget(self.titleText,alignment=Qt.AlignTop | Qt.AlignCenter)
         self.loginLayout.addWidget(self.dLogin)
-        self.loginLayout.addStretch
+        self.loginLayout.addStretch()
         
 
         self.animLogin = QPropertyAnimation(self, b"pos")
         self.animLogin.setDuration(1500)
         self.animLogin.setStartValue(QPoint(self.window.pos().x() - 300,self.pos().y() + 40))
-        self.animLogin.setEndValue(QPoint(self.window.xLogin + 45,self.pos().y() + 40))
+        self.animLogin.setEndValue(QPoint(self.window.xLogin + 51,self.pos().y() + 50))
 
+        self.animLoginFinish = QPropertyAnimation(self, b"pos")
+        self.animLoginFinish.setDuration(1500)
+        self.animLoginFinish.setStartValue(QPoint(self.pos().x(),self.pos().y() + 40))
+        self.animLoginFinish.setEndValue(QPoint(self.pos().x() - 1000,self.pos().y() + 40))
