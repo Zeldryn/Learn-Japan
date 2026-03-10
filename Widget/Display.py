005c38577a7a7627343e868a23d6951ae4d7c767
Zeldryn <ahm.riz666@gmail.com>
2026-03-10 23:39:29 +0700

    feat: add Sign Up Field (First Name,Last Name,Username,Password,Email,Birthday)

--- Widget/Display.py
+++ Widget/Display.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QLineEdit
+from PySide6.QtWidgets import QLineEdit, QSizePolicy
 from PySide6.QtCore import Qt,QPoint
 from PySide6.QtGui import QFont
 class Display(QLineEdit):
@@ -24,4 +24,52 @@ class Display(QLineEdit):
             self.setFont(self.font)
             self.setEchoMode(QLineEdit.Password)
         
+        elif self.id == "firstNameDisplay":
+            self.setPlaceholderText("First Name")
+            self.setReadOnly(False)
+            self.setAlignment(Qt.AlignCenter)
+            
+        elif self.id == "lastNameDisplay":
+            self.setPlaceholderText("Last Name")
+            self.setReadOnly(False)
+            self.setAlignment(Qt.AlignCenter)
+
+        elif self.id == "usernameRegistDisplay":
+            self.setPlaceholderText("First Name")
+            self.setReadOnly(False)
+            self.setAlignment(Qt.AlignCenter)
+            self.setSizePolicy(QSizePolicy.Expanding,QSizePolicy.Expanding)
+            self.setMinimumHeight(window.height() *0.1)
+            self.setMinimumWidth(window.width() * 0.8)
+
+        elif self.id == "passwordRegistDisplay":
+            self.setPlaceholderText("********")
+            self.setEchoMode(QLineEdit.Password)
+            self.setReadOnly(False)
+            self.setAlignment(Qt.AlignCenter)
+            self.setSizePolicy(QSizePolicy.Expanding,QSizePolicy.Expanding)
+            self.setMinimumHeight(window.height() *0.1)
+            self.setMinimumWidth(window.width() * 0.8)
+
+        elif self.id == "emailDisplay":
+            self.setPlaceholderText("YourEmail@Gmail.com")
+            self.setReadOnly(False)
+            self.setAlignment(Qt.AlignCenter)
+            self.setSizePolicy(QSizePolicy.Expanding,QSizePolicy.Expanding)
+            self.setMinimumHeight(window.height() *0.1)
+            self.setMinimumWidth(window.width() * 0.8)
+
+        elif self.id == "dayDisplay":
+            self.setPlaceholderText("0")
+            self.setReadOnly(False)
+            self.setAlignment(Qt.AlignCenter)
+
+        elif self.id == "monthDisplay":
+            self.setPlaceholderText("0")
+            self.setReadOnly(False)
+            self.setAlignment(Qt.AlignCenter)
         
+        elif self.id == "yearDisplay":
+            self.setPlaceholderText("0")
+            self.setReadOnly(False)
+            self.setAlignment(Qt.AlignCenter)
