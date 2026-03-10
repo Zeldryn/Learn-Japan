a4e7db886b809b929e580602e93c2105678bd177
Zeldryn <ahm.riz666@gmail.com>
2026-03-10 23:41:00 +0700

    feat : add Sign Up Field Text, and text after success login

--- Widget/Label.py
+++ Widget/Label.py
@@ -28,18 +28,74 @@ class Label(QLabel):
             self.animSapa.finished.connect(self.animFadeout)
             self.animSapa.start()
 
-            self.timer1 = QTimer()
-            self.timer1.timeout.connect(self.Check)
-            self.timer1.start(16)
 
         elif id == "titleLogin":
             self.setText("Login To Your Account")
             self.setStyleSheet("""QLabel {
                                color:white
                                }""")
-            self.font.setPointSize(self.window.width() * 0.15)
+            self.font.setPointSize(int(self.window.width() * 0.15))
             self.font.setItalic(True)
             self.setFont(self.font)
+
+
+        elif id == "titleRegist":
+            self.setText("Register New Account")
+            self.setStyleSheet("""QLabel {
+                               color:white
+                               }""")
+            self.font.setPointSize(15)
+            self.font.setItalic(True)
+            self.setFont(self.font)
+
+        elif id == "failLogin":
+            self.hide()
+            self.setStyleSheet("color:white")
+ 
+
+
+
+        elif id == "successLogin":
+            self.hide()
+            self.setStyleSheet("color:white;")
+            self.font.setPointSize(int(self.window.width() * 0.1))
+            self.setFont(self.font)
+
+            self.opacityEffect = QGraphicsOpacityEffect()
+            self.setGraphicsEffect(self.opacityEffect)
+            self.opacityEffect.setOpacity(0)
+
+            self.animSapa = QPropertyAnimation(self.opacityEffect,b"opacity")
+            self.animSapa.setDuration(2000)
+            self.animSapa.setStartValue(0)
+            self.animSapa.setEndValue(1)
+            self.animSapa.finished.connect(self.animFadeout)
+        
+        elif id == "textRegistFName":
+            self.setStyleSheet("color:white")
+            self.setText("First Name")
+
+        elif id == "textRegistLName":
+            self.setStyleSheet("color:white")
+            self.setText("Last Name")
+
+        elif id == "textRegistUsername":
+            self.setStyleSheet("color:white")
+            self.setText("Username")
+
+        elif id == "textRegistPassword":
+            self.setStyleSheet("color:white")
+            self.setText("Password")
+
+        elif id == "textRegistEmail":
+            self.setStyleSheet("color:white")
+            self.setText("Email Name")
+
+
+        elif id == "textRegistYear":
+            self.setStyleSheet("color:white")
+            self.setText("Birthday")
+
             
 
     def animFadeout(self):
@@ -51,14 +107,8 @@ class Label(QLabel):
 
     def delete(self): 
         self.hide()
-        self.timer1.stop()
         self.window.FullScreen()
 
 
-    def Check(self):
-        self.font.setPointSize(int(self.window.width() * 0.02))
-        self.font.setPointSize(int(self.window.height() * 0.02))
-        if self.window.width() <= 400 or self.window.height() <= 350:
-            self.font.setPointSize(int(self.width() * 0.05))
-        self.setFont(self.font)
+
             
