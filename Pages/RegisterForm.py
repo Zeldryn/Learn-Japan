9232ef5969c7e2586acf02a83ebe822fb753a14b
Zeldryn <ahm.riz666@gmail.com>
2026-03-10 23:35:03 +0700

    feat: add Register Account Form but now with Button, I Will add in the next commit

---
+++ Pages/RegisterForm.py
@@ -0,0 +1,40 @@
+from PySide6.QtWidgets import QWidget,QVBoxLayout,QHBoxLayout, QGraphicsOpacityEffect
+from PySide6.QtCore import Qt, QPropertyAnimation, QRect
+from Widget.Label import Label
+from Widget.trueRegist import trueRegist
+
+class Register(QWidget):
+    def __init__(self,window):
+        super().__init__(window)
+        self.widthAva = self.screen().availableGeometry().width()
+        self.heightAva = self.screen().availableGeometry().height()
+        self.window = window
+        self.hide()
+        self.setObjectName("mainRegist")
+        self.setAttribute(Qt.WA_StyledBackground,True)
+        self.setMinimumSize(self.window.width() * 0.3,self.window.height() * 0.8)
+        self.setStyleSheet("""#mainRegist {
+                           border : 2px none;
+
+                           border-radius: 10px;
+                           background-color: rgba(35, 76, 106, 0.6);
+                           }
+                           
+                           #mainLogin:hover {
+                           border : 2px solid rgba(69, 104, 130, 1);
+
+                           
+                           }""")
+        #Widget
+        self.rTitle = Label("titleRegist",self)
+        self.trueRegist =  trueRegist(self)
+    
+        
+        self.registLayout = QVBoxLayout(self)
+        self.registLayout.setContentsMargins(15,100,15,100)
+        self.registLayout.setSpacing(20)
+        self.registLayout.addWidget(self.rTitle,alignment=Qt.AlignTop | Qt.AlignCenter)
+        self.registLayout.addWidget(self.trueRegist,alignment=Qt.AlignCenter)
+
+
+        
