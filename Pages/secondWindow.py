dcfb42e74b5f353196e01224faa72cdc89865f39
Zeldryn <ahm.riz666@gmail.com>
2026-03-11 21:54:09 +0700

    add: Title Bar and add 3 buttons for the future modes

--- Pages/secondWindow.py
+++ Pages/secondWindow.py
@@ -1,6 +1,23 @@
 from PySide6.QtWidgets import QWidget
 
+from Widget.Dashboard import profileDashboard, MainButton, MainButton2, MainButton3
 class Dashboard(QWidget):
     def __init__(self):
         super().__init__()
+        print(self.width())
+        self.resize(self.screen().availableGeometry().width(),self.screen().availableGeometry().height())
+        self.setFixedSize(self.screen().availableGeometry().width(),self.screen().availableGeometry().height())
         self.setWindowTitle("Home")
+        self.profileDashboard = profileDashboard(self)
+        self.profileDashboard.setParent(self)
+        
+        self.mainButton = MainButton(self)
+        self.mainButton.setParent(self)
+
+        self.mainButton2 = MainButton2(self)
+        self.mainButton2.setParent(self)
+
+        self.mainButton3 = MainButton3(self)
+        self.mainButton3.setParent(self)
+
+    
