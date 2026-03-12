8a23b720f5e01c8b785c2132451c00ba5ca3ae76
Zeldryn <ahm.riz666@gmail.com>
2026-03-12 17:49:33 +0700

    feat : add Icon To Dashboard Page

--- Pages/secondWindow.py
+++ Pages/secondWindow.py
@@ -1,4 +1,5 @@
 from PySide6.QtWidgets import QWidget
+from PySide6.QtGui import QIcon
 
 from Widget.Dashboard import profileDashboard, MainButton, MainButton2, MainButton3
 class Dashboard(QWidget):
@@ -8,8 +9,10 @@ class Dashboard(QWidget):
         self.resize(self.screen().availableGeometry().width(),self.screen().availableGeometry().height())
         self.setFixedSize(self.screen().availableGeometry().width(),self.screen().availableGeometry().height())
         self.setWindowTitle("Home")
+        self.setWindowIcon(QIcon("Image/Icon/mainIcon.png"))
         self.profileDashboard = profileDashboard(self)
         self.profileDashboard.setParent(self)
+
         
         self.mainButton = MainButton(self)
         self.mainButton.setParent(self)
